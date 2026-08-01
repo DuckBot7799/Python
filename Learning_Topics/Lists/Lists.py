03f9713eb8f948cb4e5febad327d8116ebf1be6a
DuckBot7799 <193609475+DuckBot7799@users.noreply.github.com>
2026-08-01 07:42:19 +0530

    If and Else in lists.

--- Learning_Topics/Lists/Lists.py
+++ Learning_Topics/Lists/Lists.py
@@ -3,3 +3,15 @@ print(len(list))
 
 group = [True, False, False]
 print(group)
+
+
+group2 = ["123", "456", "789"]
+print(group2[-3])
+
+
+
+thislist = ["apple", "banana", "cherry", "kiwi", "mango"]
+if "cherry" in thislist:
+    print("Yes, cherry is available")
+else:
+    print("No, cherry is not available")
