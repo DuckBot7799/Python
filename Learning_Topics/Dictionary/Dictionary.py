86c9b042d3ae835ab24e6e1ba6c9c379e867d45b
DuckBot7799 <193609475+DuckBot7799@users.noreply.github.com>
2026-08-30 15:42:21 +0530

    Learnt creation of dictionary and how to access value by putting key.

---
+++ Learning_Topics/Dictionary/Dictionary.py
@@ -0,0 +1,21 @@
+# Dictionaries are used to store data values in key:value pairs.
+#
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+
+dict = {
+  "brand": "Ferrari",
+  "model": "SF90",
+  "year": 2019
+}
+print(dict)
+
+# Accessing Items
+# You can access the items of a dictionary by referring to its key name, inside square brackets:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
