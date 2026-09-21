33657a34949292dccffc9638620f8ce68bd32389
DuckBot7799 <193609475+DuckBot7799@users.noreply.github.com>
2026-09-21 20:03:20 +0530

    Copied from my main learning if-else file, but I consider this to be my another mini project on basic decision making. (if-else & elif)

---
+++ Projects/Number Compare Program #2 - After Actual Learning/Num_Compare_Program.py
@@ -0,0 +1,24 @@
+# Here is my another number compare program which is very simple made by me
+
+import time
+
+time.sleep(1)
+print("Loading...")
+
+time.sleep(2)
+a = input("Enter Your First Number:       ")
+time.sleep(1)
+b = input("Enter Your Second Number:    ")
+time.sleep(1)
+print("")
+print("Comparing Your Given Values...")
+time.sleep(2)
+
+if a > b:
+    print(f"Your First Number Is Greater! {a}")
+
+elif a < b:
+    print(f"Your Second Number Is Greater! {b}")
+
+elif a == b:
+    print(f"Your Both Given Numbers are equal! {a} and {b}")
